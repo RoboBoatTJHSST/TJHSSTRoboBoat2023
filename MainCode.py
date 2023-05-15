15256c9b307c314649c4954219957adb432f3e8c
Tianhao23 <98424854+Tianhao23@users.noreply.github.com>
2023-05-14 21:48:20 -0400

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -21,23 +21,12 @@ def remove_tags(html):
         data.decompose()
     # return data by retrieving the tag content
     return ' '.join(soup.stripped_strings)
-#print(remove_tags(html))
 """initialize the necessary things first, probably a boat object"""
-while True: #do everything in the while loop. Whenever groundstation sends us information, such as which task we're doing, perform the task in the while loop
-    # the loop will end when groundstation sends a certain type of data.
-    """
-    groundstation will be a webserver on the jetson, so there will be a python function that will access the webserver and return
-    the task number that we need to do. that means there will be another python file that this code needs to import, and the python function
-    is something that lauren needs to do.
-    """
+while True: 
     task = getTask('enter url')
     if task: #if there is a task to do
-        #Run Yolo once to detect relevant objects
-        # CV: send image data from camera: output pixels of buoys and their respective colors (dict{pixel: color})
-        # ideally, cv should take the border of the buoys and average them to a single pixel value that represents the 
-        # center of the buoy and assigns it to that color
         opt = detect.parse_opt()
-        rawoutput = detect.main(opt) #buoys = {("centerCoord","radius" ): 'color'} return format of YOLO
+        rawoutput = detect.main(opt)
         while rawoutput: #while there are still buoys/relevant objects:
             buoys = {}
             for x in rawoutput:
@@ -53,8 +42,7 @@ while True: #do everything in the while loop. Whenever groundstation sends us in
                 pt =limap.find_sphere(data[0], data[1], data[2], data[3])
                 convertedbuoydata[pt] = buoys[(data[0], data[1], data[2], data[3])]
             limap.end()
-            #using the buoy location and color data collected from CV and lidar, call task manager to output 3d points that
-            #the boat needs to go to
+            #using the buoy location and color data collected from CV and lidar, call task manager to output 3d points that the boat needs to go to
             convertedbuoydata = {(3,1,2): "Green", (5,6,1): "Red", (1,5,2): "Red", (2,6,4): "Black", (2,5,3): "Green", (5,2,6): "Yellow"}
             waypt = tm.task1(convertedbuoydata)
             print(waypt)
